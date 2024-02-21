e5cf37d17113e59e70bc5c5e36f170d6c7e891a3
tylerchiu <157323374+tylerchiu@users.noreply.github.com>
2024-02-20 20:01:12 -0700

    adding navigation links

--- streamlit_template/main_page.py
+++ streamlit_template/main_page.py
@@ -3,4 +3,7 @@ import streamlit as st
 st.markdown("# Welcome to my Website!")
 st.sidebar.markdown("# Main Page")
 
-st.write("Click on a page to see racer stats")
+st.write("Click on a page to see racer or kart stats")
+
+link = '[To My GitHub Pages Site](https://tylerchiu.github.io/GitHubTest/)'
+st.markdown(link, unsafe_allow_html=True)
